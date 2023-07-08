80116e98aa5b9a46fe9b76afaff8dd51d7a67b6a
Anuwat Thisuka <43104719+AnuwatThisuka@users.noreply.github.com>
2023-07-08 17:01:29 +0700

    dev

--- src/components/common/root-layout.tsx
+++ src/components/common/root-layout.tsx
@@ -1,12 +1,18 @@
 import { FC } from "react";
 import SiteHeader from "./site-header";
 import { Outlet } from "react-router-dom";
+import { useWindowSize } from "@/hooks/use-window-size";
 
 const RootLayout: FC = () => {
+  const { height: windowHeight } = useWindowSize();
   return (
-    <div className="r relative flex min-h-screen flex-col">
+    <div
+      className={`relative flex flex-col h-[${windowHeight}px]  overflow-hidden`}
+    >
       <SiteHeader />
-      <div className="box-border h-[calc(100vh-4rem-2rem-1px)] overflow-auto">
+      <div
+        className={`box-border h-[calc(${windowHeight}px-4rem-2rem-1px)] overflow-auto`}
+      >
         <Outlet />
       </div>
       <div className="flex h-8 items-center justify-center border-t ">
